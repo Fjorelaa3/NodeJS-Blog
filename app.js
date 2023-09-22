c047472c04100c57a812caded279fbf2b0c498f1
Fjorelaa3 <dricifjorela@gmail.com>
2023-09-22 10:46:57 +0200

    push

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninja:netninja123@cluster0.oitpyxb.mongodb.net/node-course?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
+
+
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+
+app.use('/blogs', blogRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
